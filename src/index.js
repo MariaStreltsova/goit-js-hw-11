34c3accefa6ce8e5c7236ae0265bb1c7de381519
MariaStreltsova <Mariastreltova@gmail.com>
2022-05-22 01:11:44 +0300

    update index js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './sass/main.scss';
 // import axios from "axios";
 import PhotosApiService from './js/photo-service';
 import hitsTpl from './templates/hits.hbs'
+
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(hitsTpl)
 const refs = {
     searchForm: document.querySelector('.search-form'),
@@ -10,23 +16,80 @@ const refs = {
 }
 const photosApiService = new PhotosApiService();
 
-refs.searchForm.addEventListener('submit', onSearch);
-refs.onLoadMoreBtn.addEventListener('click', onLoadMore)
+const lightbox = new SimpleLightbox('.ligtbox-link', {
+overlayOpacity: 0.4,
+animationSpeed: 100,
+    });
 
-function onSearch(e) {
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.onLoadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onSearch(e) {
     e.preventDefault();
     clearHitsGallery();
+    
+ photosApiService.resetPage();
     photosApiService.query = e.currentTarget.elements.searchQuery.value;
-    photosApiService.resetPage();
-    photosApiService.fetchPhotos().then(appendHitsMarkup);
+    if (photosApiService.query === '') {
+        chekInput();
+        return;
+    }
+  
+
+    try {
+        const result = await photosApiService.fetchPhotos();
+        appendHitsMarkup(result);
+
+
+    photosApiService.setTotalHits(result.totalHits); 
+
+        lightbox.refresh();
+        
+        Notiflix.Notify.success(`Hooray! We found ${photosApiService.totalHits} images.`);
+        onShowLoadMoreBtn();
+        onLastPhotos();
+    }
+    catch (error) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
 }
-function onLoadMore() {
-    photosApiService.fetchPhotos().then(appendHitsMarkup);
+async function onLoadMore() {
+   
+    const result = await photosApiService.fetchPhotos();
+     appendHitsMarkup(result);
+     onLastPhotos();
+      photosApiService.lastTotalHits();
+  lightbox.refresh();
 }
 
-function appendHitsMarkup(hits) {
-    refs.gallery.insertAdjacentHTML('beforeend', hitsTpl(hits) )
+function appendHitsMarkup(markup) {
+    refs.gallery.insertAdjacentHTML('beforeend', hitsTpl(markup) )
 }
 function clearHitsGallery() {
     refs.gallery.innerHTML = '';
 }
+
+function onShowLoadMoreBtn() {
+    refs.onLoadMoreBtn.classList.remove('is-hidden');
+}
+function hideShowMoreBtn() {
+    refs.onLoadMoreBtn.classList.add('is-hidden');
+}
+
+function chekInput() {
+    
+    Notiflix.Notify.failure('Sorry! You have to enter something.Please, try again!') 
+    hideShowMoreBtn();
+    photosApiService.resetPage(); 
+    }
+   
+function onLastPhotos() {
+        if (photosApiService.totalHits <= 40) {
+    hideShowMoreBtn();
+  Notiflix.Notify.info("We're sorry, but you've reached the end of search results");
+    return;
+  }
+    }
