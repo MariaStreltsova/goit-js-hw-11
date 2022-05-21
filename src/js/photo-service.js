773ec9bf1e43763c68e8f3056254a1a77ac40fc6
MariaStreltsova <Mariastreltova@gmail.com>
2022-05-22 01:11:25 +0300

    update async/await .js

--- src/js/photo-service.js
+++ src/js/photo-service.js
@@ -1,24 +1,27 @@
 import axios from "axios";
+
 export default class PhotosApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
+         this.totalHits = null;
     }
-    fetchPhotos() {
+
+    async fetchPhotos() {
+    const API_KEY = '27511871-af3c65d931511896211490940';
+    const BASE_URL = 'https://pixabay.com';
         console.log(this);
-       return axios(`https://pixabay.com/api/?key=27511871-af3c65d931511896211490940&q=${this.searchQuery}&image_type=photo&orientation=horisontal&safesearch=true&per_page=40&page=${this.page}`)
-            .then(response =>  
-            {
-                 console.log(response.data.hits) 
-                this.page += 1;
-                console.log(response.data)
-                return response.data.hits;
-                // const { hits, totalHits } = response.data;
-                // return {hits, totalHits};
+    const response =  await axios(`${BASE_URL}/api/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horisontal&safesearch=true&per_page=40&page=${this.page}`)
+        if (!response.data.total) {
+            throw new Error('error');
+        }
+        console.log(response);
+         this.page += 1;
+                const { hits, totalHits } = response.data;
+                return { hits, totalHits };
+     
             }
-            );
-        
-    }
+
     resetPage() {
         this.page = 1;
     }
@@ -30,4 +33,11 @@ export default class PhotosApiService {
          this.searchQuery = newQuery;
     }
 
+      setTotalHits(hits) {
+    this.totalHits = hits;
+  }
+
+  lastTotalHits() {
+    this.totalHits -= 40;
+  }
     }
